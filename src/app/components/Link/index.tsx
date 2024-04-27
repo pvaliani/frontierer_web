b7027e093c253018d77d7bfe1c3d9d801133a4b7
Pedram Valiani <pedram.valiani@bbc.co.uk>
2024-04-27 20:45:54 +0100

    add social icons even tho it looks gammy af

--- src/app/components/Link/index.tsx
+++ src/app/components/Link/index.tsx
@@ -1,4 +1,9 @@
+import { faFacebook, faSpotify } from "@fortawesome/free-brands-svg-icons";
 import { links, Link as SocialLink } from "./socialLinks";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBandcamp } from "@fortawesome/free-brands-svg-icons/faBandcamp";
+import { faApple } from "@fortawesome/free-brands-svg-icons/faApple";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons/faInstagram";
 
 const Link = () => {
   return (
@@ -6,19 +11,21 @@ const Link = () => {
       {links.map((link, index) => (
         <div key={index}>
           <a href={link.appleMusic} target="_blank" rel="noopener noreferrer">
-            Apple Music
+            Apple
+            <FontAwesomeIcon icon={faApple} size="lg" />
           </a>
           <a href={link.bandcamp} target="_blank" rel="noopener noreferrer">
             Bandcamp
+            <FontAwesomeIcon icon={faBandcamp} size="lg" />
           </a>
           <a href={link.facebook} target="_blank" rel="noopener noreferrer">
-            Facebook
+            <FontAwesomeIcon icon={faFacebook} size="lg" />
           </a>
           <a href={link.instagram} target="_blank" rel="noopener noreferrer">
-            Instagram
+            <FontAwesomeIcon icon={faInstagram} size="lg" />
           </a>
           <a href={link.spotify} target="_blank" rel="noopener noreferrer">
-            Spotify
+            <FontAwesomeIcon icon={faSpotify} size="lg" />
           </a>
         </div>
       ))}
