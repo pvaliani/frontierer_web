027629402eca2757771acd970fe26cea4cdba3d3
Pedram Valiani <pedram.valiani@bbc.co.uk>
2024-04-27 20:01:37 +0100

    update svg size

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -3,7 +3,7 @@ import FrontiererLogo from "../../assets/frontierer_logo.svg";
 const Header = () => {
   return (
     <header className="w-full flex justify-center items-start pt-4">
-      <FrontiererLogo />
+      <FrontiererLogo style={{ transform: "scale(0.5)" }} />
     </header>
   );
 };
