7088ed07a73ead1f952894f4417cd17a134d6e9b
Pedram Valiani <pedram.valiani@bbc.co.uk>
2024-04-28 16:41:38 +0100

    adds styling to table innit

--- src/app/tour/page.tsx
+++ src/app/tour/page.tsx
@@ -1,9 +1,29 @@
 import Layout from "../components/layout";
+import tourDates from "./tourDates";
 
 const Tour = () => {
   return (
     <Layout>
-      <p>This is the tour page innit gigs etc</p>
+      <div className="container mx-auto px-4">
+        <table className="w-full mt-8">
+          <thead className="bg-gray-200 text-black">
+            <tr>
+              <th className="text-left py-2 px-4">Date</th>
+              <th className="text-left py-2 px-4">Location</th>
+              <th className="text-left py-2 px-4">Venue</th>
+            </tr>
+          </thead>
+          <tbody>
+            {tourDates.map((tourDate, index) => (
+              <tr key={index} className="border-b">
+                <td className="py-2 px-4">{tourDate.date}</td>
+                <td className="py-2 px-4">{tourDate.location}</td>
+                <td className="py-2 px-4">{tourDate.venue}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </Layout>
   );
 };
--- src/app/tour/tourDates.ts
+++ src/app/tour/tourDates.ts
@@ -1,32 +1,30 @@
 type TourDate = {
-    day: string;
     date: string;
-    city: string;
-    country: string;
+    location: string
     venue: string;
 };
 
 // placeholder tour dates which could come from a cdn/api later on
 const tourDates: TourDate[] = [
-    { day: "Wednesday", date: "03/07/19", city: "Glasgow", country: "UK", venue: "Hug & Pint" },
-    { day: "Thursday", date: "04/07/19", city: "London", country: "UK", venue: "Boston Music Rooms" },
-    { day: "Friday", date: "05/07/19", city: "Le Havre", country: "France", venue: "VENUE TBC" },
-    { day: "Saturday", date: "06/07/19", city: "Lyon", country: "France", venue: "Le Farmer" },
-    { day: "Sunday", date: "07/07/19", city: "Milan", country: "Italy", venue: "Viale Toscana 31" },
-    { day: "Monday", date: "08/07/19", city: "Bern", country: "Switzerland", venue: "Die Cafete" },
-    { day: "Tuesday", date: "09/07/19", city: "Salzburg", country: "Austria", venue: "Rockhouse" },
-    { day: "Wednesday", date: "10/07/19", city: "Vienna", country: "Austria", venue: "Venster 99" },
-    { day: "Thursday", date: "11/07/19", city: "Budapest", country: "Hungary", venue: "Dürer Kert" },
-    { day: "Friday", date: "12/07/19", city: "Cluj-Napoca", country: "Romania", venue: "Flying Circus" },
-    { day: "Saturday", date: "13/07/19", city: "Periam", country: "Romania", venue: "Rock La Mures Festival" },
-    { day: "Sunday", date: "14/07/19", city: "Bratislava", country: "Slovakia", venue: "Kulturák Klub" },
-    { day: "Monday", date: "15/07/19", city: "Prague", country: "Czech Republic", venue: "Modra Vopice" },
-    { day: "Tuesday", date: "16/07/19", city: "Leipzig", country: "Germany", venue: "Neumanns" },
-    { day: "Wednesday", date: "17/07/19", city: "Cologne", country: "Germany", venue: "Sonic Ballroom" },
-    { day: "Thursday", date: "18/07/19", city: "Hamburg", country: "Germany", venue: "Bar 227 - possible upgrade" },
-    { day: "Friday", date: "19/07/19", city: "Eernegem", country: "Belgium", venue: "B52" },
-    { day: "Saturday", date: "20/07/19", city: "Paris", country: "France", venue: "Le Cirque Electrique" },
-    { day: "Sunday", date: "21/07/19", city: "Leeds", country: "UK", venue: "The Key Club" }
+  { date: "Wednesday 03/07/19", location: "Glasgow, UK", venue: "Hug & Pint" },
+  { date: "Thursday 04/07/19", location: "London, UK", venue: "Boston Music Rooms" },
+  { date: "Friday 05/07/19", location: "Le Havre, France", venue: "VENUE TBC" },
+  { date: "Saturday 06/07/19", location: "Lyon, France", venue: "Le Farmer" },
+  { date: "Sunday 07/07/19", location: "Milan, Italy", venue: "Viale Toscana 31" },
+  { date: "Monday 08/07/19", location: "Bern, Switzerland", venue: "Die Cafete" },
+  { date: "Tuesday 09/07/19", location: "Salzburg, Austria", venue: "Rockhouse" },
+  { date: "Wednesday 10/07/19", location: "Vienna, Austria", venue: "Venster 99" },
+  { date: "Thursday 11/07/19", location: "Budapest, Hungary", venue: "Dürer Kert" },
+  { date: "Friday 12/07/19", location: "Cluj-Napoca, Romania", venue: "Flying Circus" },
+  { date: "Saturday 13/07/19", location: "Periam, Romania", venue: "Rock La Mures Festival" },
+  { date: "Sunday 14/07/19", location: "Bratislava, Slovakia", venue: "Kulturák Klub" },
+  { date: "Monday 15/07/19", location: "Prague, Czech Republic", venue: "Modra Vopice" },
+  { date: "Tuesday 16/07/19", location: "Leipzig, Germany", venue: "Neumanns" },
+  { date: "Wednesday 17/07/19", location: "Cologne, Germany", venue: "Sonic Ballroom" },
+  { date: "Thursday 18/07/19", location: "Hamburg, Germany", venue: "Bar 227" },
+  { date: "Friday 19/07/19", location: "Eernegem, Belgium", venue: "B52" },
+  { date: "Saturday 20/07/19", location: "Paris, France", venue: "Le Cirque Electrique" },
+  { date: "Sunday 21/07/19", location: "Leeds, UK", venue: "The Key Club" }
 ];
 
 export default tourDates;
