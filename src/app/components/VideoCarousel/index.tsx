608070b1f2e8cf10bb3c4e2e80edeb766e6b4536
Pedram Valiani <pedram.valiani@bbc.co.uk>
2024-04-28 21:15:42 +0100

    add video carousel - working

--- src/app/components/VideoCarousel/index.tsx
+++ src/app/components/VideoCarousel/index.tsx
@@ -1,11 +1,10 @@
-// VideoCarousel.tsx
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination, Autoplay } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 
-const VideoCarousel: React.FC = () => {
+const VideoCarousel = () => {
   const swiperConfig = {
     modules: [Autoplay, Pagination, Navigation],
     spaceBetween: 30,
