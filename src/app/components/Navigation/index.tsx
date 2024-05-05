4765c37a5a087dfe5b588ad71101d3154755ca11
Pedram Valiani <pedram.valiani@bbc.co.uk>
2024-05-05 17:51:02 +0100

    update fonts, add store nav options

--- src/app/components/Navigation/index.tsx
+++ src/app/components/Navigation/index.tsx
@@ -1,29 +1,57 @@
+"use client";
 import Link from "next/link";
+import { useState } from "react";
 
 const Navigation = () => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
   return (
-    <nav className="flex space-x-4 p-4">
-      <Link
-        href="/home"
-        className="text-m md:text-2xl lg:text-3xl text-center text-white hover:text-gray-300"
-        passHref
-      >
+    <nav className="flex space-x-4 p-4 relative font-IBMPlexMono">
+      <Link href="/home" className="link" passHref>
         Home
       </Link>
-      <Link
-        href="/tour"
-        className="text-m md:text-2xl lg:text-3xl text-center text-white hover:text-gray-300"
-        passHref
-      >
+      <Link href="/tour" className="link" passHref>
         Tour
       </Link>
-      <Link
-        href="/store"
-        className="text-m md:text-2xl lg:text-3xl text-center text-white hover:text-gray-300"
-        passHref
+      <div
+        className="link relative"
+        onMouseEnter={() => setIsDropdownOpen(true)}
+        onMouseLeave={() => setIsDropdownOpen(false)}
       >
         Store
-      </Link>
+        {isDropdownOpen && (
+          <div className="absolute top-full left-0 bg-white text-black w-max mt-2 rounded-md shadow-lg">
+            <Link
+              href="https://frontierer.bandcamp.com"
+              target="_blank"
+              className="dropdown-item"
+            >
+              Bandcamp
+            </Link>
+            <Link
+              href="https://frontierer.bandcamp.com"
+              target="_blank"
+              className="dropdown-item"
+            >
+              US Store
+            </Link>
+            <Link
+              href="https://frontierer.bandcamp.com"
+              target="_blank"
+              className="dropdown-item"
+            >
+              Australia Store
+            </Link>
+            <Link
+              href="https://frontierer.bandcamp.com"
+              target="_blank"
+              className="dropdown-item"
+            >
+              EU Store
+            </Link>
+          </div>
+        )}
+      </div>
     </nav>
   );
 };
